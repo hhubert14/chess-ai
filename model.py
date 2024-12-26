8bf16065403eccb141ee342d4ace023d8bfffbc3
hhubert14 <h.hubert@wustl.edu>
2024-12-26 00:04:15 -0500

    Update model.py and test_dataset.csv for improved data handling and model training

--- model.py
+++ model.py
@@ -4,45 +4,47 @@ from torch.utils.data import DataLoader
 from torch.optim import AdamW
 from accelerate.test_utils.testing import get_backend
 from tqdm.auto import tqdm
-from torch.amp import autocast, GradScaler
+
 
 import torch
 # import evaluate
 
-model_name = "google/flan-t5-large"
+model_name = "google/flan-t5-base"
 batch_size = 1
-
-scaler = GradScaler()
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-raw_dataset = load_dataset("csv", data_files={"full": "/kaggle/input/games-data/games_data.csv"})["full"]
+train_dataset = load_dataset("csv", data_files={"full": "/kaggle/input/adsfdaw/train_dataset.csv"}, streaming=True)["full"]
+eval_dataset = load_dataset("csv", data_files={"full": "/kaggle/input/adsfdaw/test_dataset.csv"}, streaming=True)["full"]
 # print("Raw dataset features:", raw_dataset.features)
 
-split_dataset = raw_dataset.train_test_split(test_size=0.2, seed=42)
+# split_dataset = raw_dataset.train_test_split(test_size=0.2, seed=42)
 # print(split_dataset)
 def tokenize_function(examples):
     # inputs = tokenizer(examples["inputs"], truncation=True, padding="max_length", max_length=512)
     # labels = tokenizer(examples["label"], truncation=True, padding="max_length", max_length=512)
-    inputs = tokenizer(examples["inputs"], truncation=False, padding=True)
-    labels = tokenizer(examples["label"], truncation=False, padding=True)
+    inputs = tokenizer(examples["inputs"], truncation=True, padding=True)
+    labels = tokenizer(examples["label"], truncation=True, padding=True)
 
     inputs["labels"] = labels["input_ids"]
     return inputs
 
-tokenized_dataset = split_dataset.map(tokenize_function, batched=True)
+train_tokenized_dataset = train_dataset.map(tokenize_function, batched=True)
+eval_tokenized_dataset = eval_dataset.map(tokenize_function, batched=True)
 # Potentially remove inputs col
-tokenized_dataset = tokenized_dataset.remove_columns(["inputs", "label"])
+train_tokenized_dataset = train_tokenized_dataset.remove_columns(["inputs", "label"])
+eval_tokenized_dataset = eval_tokenized_dataset.remove_columns(["inputs", "label"])
 # tokenized_dataset = tokenized_dataset.remove_columns(["inputs"])
 # tokenized_dataset = tokenized_dataset.rename_column("label", "labels")
-tokenized_dataset.set_format("torch")
-print(tokenized_dataset)
+train_tokenized_dataset.set_format("torch")
+eval_tokenized_dataset.set_format("torch")
+# print(tokenized_dataset)
 
 data_collator = DataCollatorWithPadding(tokenizer)
 
-train_dataloader = DataLoader(tokenized_dataset["train"], shuffle=True, batch_size=batch_size, collate_fn=data_collator)
-eval_dataloader = DataLoader(tokenized_dataset["test"], batch_size=batch_size, collate_fn=data_collator)
+train_dataloader = DataLoader(train_tokenized_dataset, shuffle=True, batch_size=batch_size, collate_fn=data_collator)
+eval_dataloader = DataLoader(eval_tokenized_dataset, batch_size=batch_size, collate_fn=data_collator)
 
 optimizer = AdamW(model.parameters(), lr=5e-5)
 
@@ -63,12 +65,12 @@ for epoch in range(num_epochs):
     for batch in train_dataloader:
         batch = {k: v.to(device) for k, v in batch.items()}
 
-        with autocast():
-            outputs = model(**batch)
-            loss = outputs.loss
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.step(lr_scheduler)
+        outputs = model(**batch)
+        loss = outputs.loss
+        loss.backward()
+
+        optimizer.step()
+        lr_scheduler.step()
         optimizer.zero_grad()
         progress_bar.update(1)
 
